1ed9f64b00e3a1b01fa91a61aa8f78b87e52262c
chrisspenceratx <chrisspenceratx@gmail.com>
2022-08-24 13:29:29 -0500

    added error handling function, morgan log function

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express'),
-  morgan = require('morgan');
-
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+// morgan middleware function to log requests to terminal//
+app.use(morgan('common'));
+
+// directs visits to public foler//
+app.use(express.static('public'));
+
+// variable declared for movie list//
 let topMovies = [
   {title: 'Goodfellas' },
   {title: 'Godfather 2'},
@@ -14,18 +20,24 @@ let topMovies = [
   {title: 'Dumb and Dumber'},
   {title: 'Shawshank Redemption'},
   {title: 'A Streetcar Named Desire'}
-
-
 ];
-
-app.use(morgan('common'));
 
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
 
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
 app.get('/secreturl', (req, res) => {
   res.send('This is a secret url with super top-secret content.');
+});
+
+// Error handling middleware function//
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(8080, () => {
