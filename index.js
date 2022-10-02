5a130c37905d459b209064c07002e7338ae2e910
chrisspenceratx <chrisspenceratx@gmail.com>
2022-10-02 07:02:45 -0500

    comment out /movies functions..auth for /movies

--- index.js
+++ index.js
@@ -279,39 +279,50 @@ app.get('/users/:Username', (req, res) => {
     });
   });
 
-app.get('/movies', (req, res) => {
+// app.get('/movies', (req, res) => {
+//   Movies.find()
+//     .then((movies) => {
+      
+//       res.status(201).json(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error: " + err);
+//     });
+// });
+
+// app.get('/movies/:Title', (req, res) => {
+//   Movies.findOne({Title: req.params.Title })
+//     .then((movie) => {
+      
+//       res.json(movie);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error: " + err);
+//     });
+// });
+
+// app.get('/movies/genres/:Genre', (req, res) => {
+//   Movies.find({ "Genre.Name": req.params.Genre })
+//     .then((movies) => {
+//       res.send(movies);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(400).send('Error: ' + err);
+//     })
+// });
+
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
-      
       res.status(201).json(movies);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
     });
-});
-
-app.get('/movies/:Title', (req, res) => {
-  Movies.findOne({Title: req.params.Title })
-    .then((movie) => {
-      
-      res.json(movie);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
-
-app.get('/movies/genres/:Genre', (req, res) => {
-  Movies.find({ "Genre.Name": req.params.Genre })
-    .then((movies) => {
-      res.send(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).send('Error: ' + err);
-    })
 });
 
 // Returns the description of a Genre (of the first one found)
