c2f1aa247da6b78d5c98edc0f829938e73da64e7
chrisspenceratx <chrisspenceratx@gmail.com>
2022-10-14 05:54:35 -0500

    connecting database to my API on Heroku

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 // morgan middleware function to log requests to terminal//
